f264aa26ce719c6207c960844e05cd589c65067e
Abhishek <abhishekjaison04@gmail.com>
2024-07-06 16:57:26 +0530

    Deploy to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "62a609de175ba47d07a3995722996c2f
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "a902311626ebafb31bae7d5c4640d896",
+"flutter_bootstrap.js": "19b71121dd8f3c926a1f752cfe694cfa",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
