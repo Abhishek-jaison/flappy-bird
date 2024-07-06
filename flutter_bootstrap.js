cb208d4bb996b3394bbcd1827f767f0a1f964f30
Abhishek <abhishekjaison04@gmail.com>
2024-07-06 16:44:41 +0530

    Deploy to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1657546939"
+    serviceWorkerVersion: "177291372"
   }
 });
